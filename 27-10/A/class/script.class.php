d91ff3e4733815bd3c7175eef37d44b8eac917fa
Mr-R4F <rafael.soares0777@gmail.com>
2022-11-17 12:29:10 -0300

    subindpo

--- 27-10/A/class/script.class.php
+++ 27-10/A/class/script.class.php
@@ -1,23 +1,33 @@
 <?php
 class Retangulo {
-protected $m_largura;
-protected $m_altura;
-public function setLargura ( $largura )
-{ $this->m_largura = $largura;}
-public function setAltura ($altura )
-{ $this->m_altura = $altura; }
-public function getArea() {
-return ($this->m_largura * $this->m_altura); }
+    protected $m_largura;
+    protected $m_altura;
+    public function setLargura ( $largura ) { 
+        $this->m_largura = $largura;
+    }
+
+    public function setAltura ($altura){ 
+        $this->m_altura = $altura; 
+    }
+
+    public function getArea() {
+        return ($this->m_largura * $this->m_altura); 
+    }
 }
+
 class Quadrado extends Retangulo {
-public function setLargura($largura) {
-$this->m_largura = $largura;
-$this->m_altura = $largura;
+    public function setLargura($largura) {
+            $this->m_largura = $largura;
+            $this->m_altura = $largura;
+    }
+    
+    public function setAltura ($altura) {
+        $this->m_largura = $altura;
+        $this->m_altura = $altura;
+    } 
 }
-public function setAltura ($altura) {
-$this->m_largura = $altura;
-$this->m_altura = $altura;
-} }
+
 $q1 = new Quadrado;
 $q1->setAltura(10);
-$q1->setLargura(5); echo $q1->getArea(); 
+$q1->setLargura(5); 
+echo $q1->getArea(); 
